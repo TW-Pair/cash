f27ed822c85c418b29658581a3410c91f70c1170
Xiaowu Feng <xiaowu.feng@rea-group.com>
2016-07-02 11:41:01 +0800

    init the java project

---
+++ test/MainTest.java
@@ -0,0 +1,20 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by xwfeng on 7/2/16.
+ */
+public class MainTest extends TestCase {
+
+  public void setUp() throws Exception {
+    super.setUp();
+
+  }
+
+  public void tearDown() throws Exception {
+
+  }
+
+  public void testMain() throws Exception {
+
+  }
+}
